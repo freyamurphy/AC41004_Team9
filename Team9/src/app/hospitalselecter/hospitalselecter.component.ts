72c03d624ff12b2047f9741d5afe68611e3cd01a
Steven Shearer <syshearer@dundee.ac.uk>
2019-09-19 15:50:38 +0100

    Changed the Listbox to List Box using Bootstrap
    Tried Material Angular; failed. Used Bootstrap, so now the ListBoxes look better, still can be improved upon. Still showing data made fron the component.ts as database connection is still not there. Next Step: Make it look better and resize so it looks good in the home screen. Hopefully get Database data in it soon as well.

--- Team9/src/app/hospitalselecter/hospitalselecter.component.ts
+++ Team9/src/app/hospitalselecter/hospitalselecter.component.ts
@@ -5,26 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './hospitalselecter.component.html',
   styleUrls: ['./hospitalselecter.component.css']
 })
+
 export class HospitalselecterComponent implements OnInit {
 
   hospitalList: Section[] = [
   {
     name: 'Hospital',
+    address: '101 Dundee Street Dundee DD1 2AS',
   },
   {
     name: 'Maybe',
+    address: '101 V&A Street Dundee DD1 2AG',
   },
   {
     name: 'This',
+    address: 'University Dundee DD2 3AJ',
   },
   {
     name: 'Will',
+    address: 'Not doing anymore Dundee DD4 1RC',
   },
   {
     name: 'Work',
+    address: '101 V&A Street Dundee DD1 2AG',
   },
   {
     name: 'Maybe',
+    address: '101 V&A Street Dundee DD1 2AG',
   },
 ];
 
