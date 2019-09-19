81fb442a8968aebf81e246c0375ba8ac3f907148
zhendong liu <zzvliu@dundee.ac.uk>
2019-09-19 13:36:46 +0100

    Fix search function
    Fix search function

--- Team9/src/app/mapoutlineareas-test/mapoutlineareas-test.component.ts
+++ Team9/src/app/mapoutlineareas-test/mapoutlineareas-test.component.ts
@@ -23,7 +23,7 @@ export class MapoutlineareasTESTComponent implements OnInit {
         this.innerHeight= window.innerHeight;
   }
 
-
+// Sort function
  sortTable(n) {
          var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
          table = document.getElementById("myTable");
@@ -78,6 +78,33 @@ export class MapoutlineareasTESTComponent implements OnInit {
              }
          }
      }
+
+//Search function
+     searchFunction() {
+             // Declare variables
+             var input, filter, table, tr, td, i, txtValue;
+             input = document.getElementById("myInput");
+             filter = input.value.toUpperCase();
+             table = document.getElementById("myTable");
+             tr = table.getElementsByTagName("tr");
+
+             // Loop through all table rows, and hide those who don't match the search query
+             for (i = 0; i < tr.length; i++) {
+                 td = tr[i].getElementsByTagName("td")[3];
+
+                 if (td) {
+                     txtValue = td.textContent || td.innerText;
+                     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                         tr[i].style.display = "";
+                     } else {
+                         tr[i].style.display = "none";
+                     }
+                 }
+             }
+         }
+
+
+
 
 
 
