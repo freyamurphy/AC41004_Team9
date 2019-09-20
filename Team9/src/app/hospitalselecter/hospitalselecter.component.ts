cb0fbb645108786debb18df5a7fbbf23dae422a5
Steven Shearer <syshearer@dundee.ac.uk>
2019-09-20 09:34:02 +0100

    Update hospitalselecter.component.ts
    Changed DataType of the Array, so it works on everyones PC

--- Team9/src/app/hospitalselecter/hospitalselecter.component.ts
+++ Team9/src/app/hospitalselecter/hospitalselecter.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class HospitalselecterComponent implements OnInit {
 
-  hospitalList: Section[] = [
+  hospitalList: any[] = [
   {
     name: 'Hospital',
     address: '101 Dundee Street Dundee DD1 2AS',
