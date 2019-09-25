98b5d4f236424a2db1e5453032dcb3ac516dbc73
lenfam <lenard.martin.gaunt@gmail.com>
2019-09-25 13:16:23 +0100

    manual

--- Team9/src/app/manuallocation/manuallocation/manuallocation.component.ts
+++ Team9/src/app/manuallocation/manuallocation/manuallocation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manuallocation',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManuallocationComponent implements OnInit {
 
-  constructor() { }
+  zipcode : any;
+  baseUrl : string;
+  temp : any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
-
+  getLocation()
+  {
+    
+    this.zipcode = ((document.getElementById("addressM") as HTMLInputElement).value);
+    console.log("H");
+    
+    this.baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=99501&key=AIzaSyA7eaqYll1QlUO_OpGtshZQHhNbbKUjWd8";
+    
+    this.http.get(this.baseUrl).subscribe(value=>{
+      this.temp = JSON.stringify(value);
+      this.temp = JSON.parse(this.temp);
+      console.log(this.temp);
+}, error=>{ });;
+  }
 }
+
+
