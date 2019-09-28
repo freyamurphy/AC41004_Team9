f32c2aa80c896743c2198bb818de404205dfea02
ryanl13214 <rlangridge132@hotmail.com>
2019-09-28 15:39:43 +0100

    added in zhens work

--- Team9/src/app/comunication.service.ts
+++ Team9/src/app/comunication.service.ts
@@ -15,6 +15,9 @@ focusedlocation$ = this.focusedlocationSource.asObservable();
 userlat:any=0;
 userlong:any=0;
 
+usersort:any;
+
+
 private arrayofstufflocationSource = new Subject<any>();
 arrayofstuff$ = this.arrayofstufflocationSource.asObservable();
 
@@ -39,8 +42,15 @@ userstate:any;
 runsearch(code) {
 // todo make sure this runs as an * if there is no address
   console.log("runnign a sql search inside communicaton manager with code ",code);
-   console.log(this.userstate);
-  this.sqlapi.searchWithStateAndDRGCodeFunction(this.userstate,code).subscribe((res: any) => {this.arrayofstufflocationSource.next(res);this.resultlength=res.length;this.hospitalHandler(res);});
+  console.log(this.userstate);
+  this.sqlapi.searchWithStateAndDRGCodeFunction(this.userstate,code).subscribe((res: any) =>
+  {
+    this.arrayofstufflocationSource.next(res);
+    this.resultlength=res.length;
+    this.hospitalHandler(res);
+    this.usersort=res;
+    this.sortPriceFunction();
+  });
 
 
 
@@ -65,7 +75,83 @@ getautoComplete(): Observable<any> {
 
 
 
+sortPriceFunction(){
+//Set the sorting direction to ascending:
+/*
+  var temp ;
+      for(let i=0; i<this.usersort.length; i++) {
 
+          for(let j=this.usersort.length-1; j>i; j--) {
+              if(this.usersort[j].averageTotalPayments < this.usersort[j-1].averageTotalPayments)
+              {
+                //  [this.usersort[j+1], this.usersort[j]] = [this.usersort[j], this.usersort[j+1]];
+  temp=this.usersort[j-1];
+  this.usersort[j-1]=this.usersort[j];
+  this.usersort[j]=temp;
+              }
+          }
+      }
+*/
+
+console.log("zhen said hi ");
+var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+   switching = true;
+   //Set the sorting direction to ascending:
+   dir = "asc";
+   /*Make a loop that will continue until
+   no switching has been done:*/
+   while (switching) {
+       //start by saying: no switching is done:
+       switching = false;
+       rows = this.usersort;
+      // rows=this.usersort.rows;
+       /*Loop through all table rows (except the
+       first, which contains table headers):*/
+       for (i = 0; i < (rows.length - 1); i++) {
+           //start by saying there should be no switching:
+           shouldSwitch = false;
+           /*Get the two elements you want to compare,
+           one from current row and one from the next:*/
+           x = rows[i].averageTotalPayments;
+           y = rows[i + 1].averageTotalPayments;
+           /*check if the two rows should switch place,
+           based on the direction, asc or desc:*/
+           if (dir == "asc") {
+               if (x > y) {
+                   //if so, mark as a switch and break the loop:
+                   shouldSwitch= true;
+                   break;
+               }
+           } else if (dir == "desc") {
+               if (x < y) {
+                   //if so, mark as a switch and break the loop:
+                   shouldSwitch = true;
+                   break;
+               }
+           }
+       }
+       if (shouldSwitch) {
+           /*If a switch has been marked, make the switch
+           and mark that a switch has been done:*/
+           [this.usersort[i+1], this.usersort[i]] = [this.usersort[i], this.usersort[i+1]]
+           switching = true;
+           //Each time a switch is done, increase this count by 1:
+           switchcount ++;
+       } else {
+           /*If no switching has been done AND the direction is "asc",
+           set the direction to "desc" and run the while loop again.*/
+           if (switchcount == 0 && dir == "asc") {
+               dir = "desc";
+               switching = true;
+           }
+       }
+   }
+
+        return this.usersort;
+
+
+}
 
 
 
