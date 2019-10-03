72f18d73491c61ca6ee76d0bcb6e3c8019ce8b8e
ryanl13214 <rlangridge132@hotmail.com>
2019-10-03 14:58:32 +0100

    fixed here

--- Team9/src/app/geolocation/geolocation.component.ts
+++ Team9/src/app/geolocation/geolocation.component.ts
@@ -210,8 +210,6 @@ public doSelectOptions = (options: INgxSelectOption[]) =>{};
       }
       this.zipcode = this.stateValue;
 
-this.comunicate.settypeofseaech(0);
- 
     }
 
     this.baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + this.zipcode + "&key=AIzaSyA7eaqYll1QlUO_OpGtshZQHhNbbKUjWd8&region=US";
